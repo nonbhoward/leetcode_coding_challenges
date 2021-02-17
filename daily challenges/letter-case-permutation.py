c06cf974dfb407a1cc6c5a77742b091e015bd17a
dbrannonhoward <dbrannonhoward@gmail.com>
2021-02-16 20:49:19 -0500

    in progress

---
+++ daily challenges/letter-case-permutation.py
@@ -0,0 +1,28 @@
+from random import shuffle
+
+
+class Solution:
+    @staticmethod
+    def letter_case_perm(_s: str) -> list:
+        digits, lower, upper = '0123456789', 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        permutations = [_s]
+        _s = list(_s)
+        perm_count = 0
+        for i, char in enumerate(_s):
+            if char in digits:
+                continue
+            perm_count += 1
+            _s[i] = char.upper() if char in lower else char.lower()
+            permutations.append(''.join(_s))
+        assert len(permutations) == 2 ** perm_count, 'permutation missing'
+        return permutations
+
+
+test_strings = ['a', 'ab', 'abc', 'abcd']
+key = list('abcdefghij')
+meta_permutations = dict()
+for test_string in test_strings:
+    s = Solution()
+    shuffle(key)
+    meta_permutations[''.join(key)] = s.letter_case_perm(test_string)
+pass
