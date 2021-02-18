ffa1a938d31c5e21e235a9e7d4583233be70b1ad
dbrannonhoward <dbrannonhoward@gmail.com>
2021-02-17 23:15:37 -0500

    unresolved

--- daily challenges/letter-case-permutation.py
+++ daily challenges/letter-case-permutation.py
@@ -3,19 +3,29 @@ from random import shuffle
 
 class Solution:
     @staticmethod
-    def letter_case_perm(_s: str) -> list:
-        digits, lower, upper = '0123456789', 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        permutations = [_s]
-        _s = list(_s)
-        perm_count = 0
-        for i, char in enumerate(_s):
-            if char in digits:
-                continue
-            perm_count += 1
-            _s[i] = char.upper() if char in lower else char.lower()
-            # FIXME does not capture all permutations.. only some
-            permutations.append(''.join(_s))
-        assert len(permutations) == 2 ** perm_count, 'permutation missing'
+    def letter_case_perm(_string: str) -> list:
+        digits, _string = '0123456789', list(_string)
+
+        def count_non_digits(_str: list) -> tuple:
+            p_count, pols = 0, list()
+            for _chr in _str:
+                if _chr not in digits:
+                    p_count += 1
+                    pols.append(1)
+            p_total = 2 ** p_count
+            return p_total, pols
+
+        permutation_total, polarities = count_non_digits(_string)
+        permutations = list()
+        for permutation in range(1, permutation_total + 1):
+            perm = list()
+            for _i, _char in enumerate(_string):
+                try:
+                    polarities[_i] = -1 * polarities[_i] if _i % permutation == 0 else polarities[_i]
+                    perm.append(_char.upper() if polarities[_i] > 0 else _char.lower())
+                except Exception as ex:
+                    print(ex)
+            permutations.append(perm)
         return permutations
 
 
