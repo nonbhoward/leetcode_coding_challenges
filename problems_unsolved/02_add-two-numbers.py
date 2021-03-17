9a780a6a7043f9f04c33ab81fcb087e3332a19a9
nonbhowardST <55259160+nonbhoward@users.noreply.github.com>
2021-03-16 21:42:06 -0400

    setup boilerplate

---
+++ problems_unsolved/02_add-two-numbers.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, val=0, _next=None):
+        self.val = val
+        self.next = _next
+
+
+class Solution:
+    def add_two_numbers(self, ln1: ListNode, ln2: ListNode) -> ListNode:
+        pass
+
+
+_ln1 = ListNode()
+_ln2 = ListNode()
+_s = Solution()
+node_sum = _s.add_two_numbers(_ln1, _ln2)
+pass
