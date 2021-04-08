69dddb3d5bcca3b0e01ff9eed105ef4e44ddff8c
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-04-07 22:58:02 -0400

    wip: building listnodes wrong

--- problems_unsolved/02_add-two-numbers.py
+++ problems_unsolved/02_add-two-numbers.py
@@ -1,7 +1,7 @@
 class ListNode:
     def __init__(self, val=0, _next=None):
         self.val = val
-        self.next = _next
+        self.next = next
 
 
 class Solution:
@@ -9,8 +9,12 @@ class Solution:
         pass
 
 
-_ln1 = ListNode()
-_ln2 = ListNode()
-_s = Solution()
-node_sum = _s.add_two_numbers(_ln1, _ln2)
+_ln1 = ListNode(3)
+_ln1.next = 4
+_ln1.next = 2
+_ln2 = ListNode(4)
+_ln2.next = 6
+_ln2.next = 5
+_s = Solution(_ln1, _ln2)
+_ln_sum = _s.add_two_numbers(_ln1, _ln2)
 pass
