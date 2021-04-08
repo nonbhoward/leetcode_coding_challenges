6de7bdee7c99a63ab3ca05297878f3920ba732d5
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-04-07 22:57:30 -0400

    refactor: easier copy/paste

--- problems_unsolved/13_roman_to_integer.py
+++ problems_unsolved/13_roman_to_integer.py
@@ -1,10 +1,9 @@
 class Solution:
     def roman_to_int(self, s: str) -> int:
-        
         pass
 
 
 _rs = 'X'
 _s = Solution()
-int_val = _s.roman_to_int(_rs)
+_int_val = _s.roman_to_int(_rs)
 pass
