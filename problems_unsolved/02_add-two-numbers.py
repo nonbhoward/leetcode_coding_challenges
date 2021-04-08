50d78f6ff7b245b398be8def656d83f396361a77
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-04-07 23:31:49 -0400

    wip: experimenting

--- problems_unsolved/02_add-two-numbers.py
+++ problems_unsolved/02_add-two-numbers.py
@@ -6,15 +6,19 @@ class ListNode:
 
 class Solution:
     def add_two_numbers(self, ln1: ListNode, ln2: ListNode) -> ListNode:
-        pass
+        for _ in range(10):
+            print(f'value is {ln1.val}')
+            ln1 = ln1.next
 
 
-_ln1 = ListNode(3)
-_ln1.next = 4
-_ln1.next = 2
-_ln2 = ListNode(4)
-_ln2.next = 6
-_ln2.next = 5
-_s = Solution(_ln1, _ln2)
+_ln1 = ListNode(val=3,
+                _next=ListNode(val=4,
+                               _next=ListNode(val=2,
+                                              _next=None)))
+_ln2 = ListNode(val=4,
+                _next=ListNode(val=6,
+                               _next=ListNode(val=5,
+                                              _next=None)))
+_s = Solution()
 _ln_sum = _s.add_two_numbers(_ln1, _ln2)
 pass
