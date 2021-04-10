c75a4ec2af18dde08939a77b044ffd226c4a006d
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-04-10 13:57:28 -0400

    refactor: clearer names

--- problems_unsolved/02_add-two-numbers.py
+++ problems_unsolved/02_add-two-numbers.py
@@ -5,15 +5,20 @@ class ListNode:
 
 
 class Solution:
-    def add_two_numbers(self, ln1: ListNode, ln2: ListNode) -> ListNode:
-        carry, nex = False, None
+    def add_two_numbers(self, ln_addend_a: ListNode, ln_addend_b: ListNode) -> ListNode:
+        # init carry and initial ListNode object to store the sum
+        carry = 0
         ln_sum = ListNode()
-        while ln1 and ln2:
-            ln_sum.val = ln1.val + ln2.val if not carry else ln1.val + ln2.val + 1
-            carry = True if ln_sum.val > 9 else False
-            ln_sum.val = ln_sum.val - 10 if carry else ln_sum.val
-            ln1, ln2, ln_sum.next = ln1.next, ln2.next, ListNode(val=ln_sum.val,
-                                                                 _next_=None if not ln_sum.next else ln_sum.next)
+        while ln_addend_a and ln_addend_b:
+            # sum values and determine if carry required
+            sum_of_ab = ln_addend_a.val + ln_addend_b.val
+            carry = 1 if sum_of_ab > 9 else 0
+            # adjust value decimal place if carry
+            ln_sum.val = sum_of_ab + carry - 10 if carry else sum_of_ab
+            # increment each part
+            ln_addend_a, ln_addend_b = ln_addend_a.next, ln_addend_b.next
+            # ln1, ln2, ln_sum.next = ln1.next, ln2.next, ListNode(val=ln_sum.val,
+            #                                                      _next_=None if not ln_sum.next else ln_sum.next)
         return ln_sum.next
 
 
